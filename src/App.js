1c9986ccea920a3fa365261d102f2801ceae7886
Abrar-29 <wwwadnan457@gmail.com>
2024-06-08 13:54:48 +0300

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import LoanForm from "./LoanForm";
-
+// import Routing from "./Route";
 function App() {
   return (
     <div className="App" style={{ marginTop: "150px" }}>
       <LoanForm />
+      <hr></hr>
+      {/* <Routing /> */}
     </div>
   );
 }
